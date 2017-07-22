a2fc00a8bd5b895624598e3cbc0ad62fb4c8d8d4
kaxla <kaylas.email.address@gmail.com>
2017-07-21 17:08:55 -0700

    fix project validations again

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    if @project.valid?
+    if @project.save
       redirect_to projects_path, notice: 'Project was successfully created'
     else
       render action: 'new', notice: 'Something went wrong, try again'
